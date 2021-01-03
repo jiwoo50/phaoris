ffa29c757eda1702787df7420bd55ea9851f7a9d
jiwoo50 <72139485+jiwoo50@users.noreply.github.com>
2021-01-03 23:25:13 +0900

    2021-01-03 작업
    기능추가:
    - patrol enemy의 patrol 기능추가
    - patrol enemy와 player가 충돌시 넉백 추가
    기능수정:
    - player가 이동, 점프시 애니메이션  전환 유연하게 수정
    형식:
    - 코드형식변경
    	- 클래스에 파스칼 케이
    	- 변수에는 카멜케이스
    	- 메소드는 파스칼케이스
    프로젝트 :
    - develop브랜치 추가
    기타:
    - 방어막스프라이트 추가
    - 바이러스 스프라이트 추가
    - 적 스프라이트 2종류 추가
    
    2021-01-04에 커밋 메시지 수정

---
+++ JWproject/Assets/scripts/VirusAtkEnemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusAtkEnemy : MonoBehaviour
+{
+    Rigidbody2D rigidBody2D;
+    Animator animator;
+    float nextMove = 1.0f;
+    public float distance;
+    public float atkDistance;
+    public LayerMask isLayer;
+    public float speed;
+    public GameObject virus;
+    public Transform pos;
+    // Start is called before the first frame update
+    public float cooltime;
+    private float currenttime;
+    private void Awake()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody2D.velocity = new Vector2(3.0f * nextMove, rigidBody2D.velocity.y);
+        Vector2 frontVector = new Vector2(rigidBody2D.position.x + nextMove * 0.4f, rigidBody2D.position.y);
+        RaycastHit2D raycast = Physics2D.Raycast(frontVector, Vector3.down, 1, LayerMask.GetMask("map"));
+        transform.localScale = new Vector3(-1.0f * nextMove, 1.0f, 1.0f);
+        if (raycast.collider == null)
+        {
+            nextMove = nextMove * (-1);
+        }
+        
+        RaycastHit2D attackRaycast = Physics2D.Raycast(transform.position, transform.right * -1, distance, isLayer);
+        if (attackRaycast.collider != null)
+        {
+            if (Vector2.Distance(transform.position, attackRaycast.collider.transform.position) < atkDistance)
+            {
+                if (currenttime <= 0)
+                {
+                    GameObject viruscopy = Instantiate(virus, pos.position, transform.rotation);
+                    currenttime = cooltime;
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, attackRaycast.collider.transform.position, Time.deltaTime * speed);
+
+                }
+                currenttime -= Time.deltaTime;
+            }
+        }
+    }
+}
