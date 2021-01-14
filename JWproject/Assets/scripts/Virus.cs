efaf21f56f0f2b88edc3309d2345ece7516d2a4b
jiwoo50 <10622jiwoo@gmail.com>
2021-01-15 01:36:50 +0900

    Implement the enemy's long-range attack

--- JWproject/Assets/scripts/Virus.cs
+++ JWproject/Assets/scripts/Virus.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class Virus : MonoBehaviour
 {
-    public float speed;
-    public float distance;
-    public LayerMask isLayer;
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rigidbody2d;
+    float currentTime = 1.5f;
+    float damage = 1.0f;
+    void Awake()
     {
-        
-    
+        rigidbody2d = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Launch(float direction, float force)
     {
-        
+        Vector2 directionVector = new Vector2(direction, 0);
+        rigidbody2d.AddForce(directionVector * force, ForceMode2D.Force);
+    }
+
+    void FixedUpdate()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime<=0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" )
+        {
+            HealthControl healthControl = collision.gameObject.GetComponent<HealthControl>();
+            Destroy(this.gameObject);
+            healthControl.ChangeHealth(damage);
+            
+        }
+        else if (collision.gameObject.tag == "barrier" || collision.gameObject.tag == "ground")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ JWproject/Assets/scripts/VirusAttack.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusAttack : MonoBehaviour
+{
+    public GameObject VirusPrefab;
+
+    float currtime = 0;
+    float saveDirection;
+    float direction;
+    bool fireOn = false;
+    Rigidbody2D rigidbody2D;
+    PatrolEnemy patrolEnemy;
+    GameObject parent ;
+    
+    private void Awake()
+    {
+        parent = transform.parent.gameObject;
+        rigidbody2D = parent.GetComponent<Rigidbody2D>();
+        patrolEnemy = parent.GetComponent<PatrolEnemy>();
+        
+    }
+    
+    IEnumerator Fire()
+    {
+        while (fireOn)
+        {
+            GameObject projectileObject = Instantiate(VirusPrefab, rigidbody2D.position + Vector2.up * 0.5f, Quaternion.identity);
+            Virus projectile = projectileObject.GetComponent<Virus>();
+            projectile.Launch(direction, 300);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player"&&fireOn==false)
+        {
+            patrolEnemy.setDirection = 0;
+            fireOn = true;
+            if (parent.transform.position.x < collision.gameObject.transform.position.x)
+            {
+                saveDirection = parent.transform.localScale.x;
+                parent.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                direction = 1.0f;
+            }
+            else if (parent.transform.position.x > collision.gameObject.transform.position.x)
+            {
+                saveDirection = parent.transform.localScale.x;
+                parent.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                direction = -1.0f;
+            }
+            StartCoroutine("Fire");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            patrolEnemy.setDirection = patrolEnemy.SaveDirection();
+            parent.transform.localScale = new Vector3(saveDirection, 1.0f, 1.0f);
+            fireOn = false;
+            StartCoroutine("Fire");
+        }
+    }
+}
